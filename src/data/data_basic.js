68d3e8193e893163a8d1649a30871e0432b24ca0
noxHub1230 <s110907018@stu.ntue.edu.tw>
2026-05-16 20:14:00 +0800

    App.js資料搬遷

--- src/data/data_basic.js
+++ src/data/data_basic.js
@@ -25,3 +25,87 @@ export const navItems = [
     hidden:true
   },
 ];
+export const socialLinks=[
+  {
+    label:"Facebook",
+    url:"https://www.facebook.com",
+    icon:"https://img.icons8.com/?size=100&id=8818&format=png&color=d0d9be",
+    icon_hover:"https://img.icons8.com/?size=100&id=435&format=png&color=FFFFFF"
+  },
+  {
+    label:"Instagram",
+    url:"https://www.instagram.com",
+    icon:"https://img.icons8.com/?size=100&id=32309&format=png&color=d0d9be",
+    icon_hover:"https://img.icons8.com/?size=100&id=32292&format=png&color=FFFFFF"
+  },
+  {
+    label:"Threads",
+    url:"https://www.threads.net",
+    icon:"https://img.icons8.com/?size=100&id=AS2a6aA9BwK3&format=png&color=d0d9be",
+    icon_hover:"https://img.icons8.com/?size=100&id=ikThuZ5WmSYz&format=png&color=FFFFFF"
+  }
+];
+export const contactInfo=[
+  {
+    way:"聯繫方式",
+    infos:[
+    {text:"總公司：高藏州霞岐縣山田町下狛西作り道6"},
+    {text:"直營店：高藏州霞岐縣川茂町上通り1-2-3"},
+    {text:"電話：02-12345678"},
+    {text:"傳真：02-87654321"},
+    {text:"電子郵件：info@company.com"}
+    ]
+  },
+  {
+    way:"營業時間",
+    infos:[
+      {text:"週一至週五：9:00 - 18:00"},
+      {text:"週六：10:00 - 16:00"},
+      {text:"週日：休息"},
+      {text:"(特殊節假日營業時間另行公告)"},
+      {text:""}
+    ]
+  },
+  {
+    way:"導覽綱要",
+    infos:[
+      {text:"首頁",tabName:"home"},
+      {text:"資訊消息",tabName:"news"},
+      {text:"經營歷史",tabName:"history"},
+      {text:"特色產品",tabName:"products"},
+      {text:"門市據點",tabName:"contact"}
+    ]
+  }
+];
+const contactInfo=[
+  {
+    way:"聯繫方式",
+    infos:[
+    {text:"總公司：高藏州霞岐縣山田町下狛西作り道6"},
+    {text:"直營店：高藏州霞岐縣川茂町上通り1-2-3"},
+    {text:"電話：02-12345678"},
+    {text:"傳真：02-87654321"},
+    {text:"電子郵件：info@company.com"}
+    ]
+  },
+  {
+    way:"營業時間",
+    infos:[
+      {text:"週一至週五：9:00 - 18:00"},
+      {text:"週六：10:00 - 16:00"},
+      {text:"週日：休息"},
+      {text:"(特殊節假日營業時間另行公告)"},
+      {text:""}
+    ]
+  },
+  {
+    way:"導覽綱要",
+    infos:[
+      {text:"首頁",tabName:"home"},
+      {text:"資訊消息",tabName:"news"},
+      {text:"經營歷史",tabName:"history"},
+      {text:"特色產品",tabName:"products"},
+      {text:"門市據點",tabName:"contact"}
+    ]
+  }
+];
