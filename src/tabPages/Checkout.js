312882fdb3b74fd98c916d0fe1b158b91f027199
noxHub1230 <s110907018@stu.ntue.edu.tw>
2026-05-16 23:29:17 +0800

    新增文字樣式

--- src/tabPages/Checkout.js
+++ src/tabPages/Checkout.js
@@ -10,27 +10,27 @@ export default function Cart() {
   return (
     <div id="BG_checkout" className="tab-page container-fluid p-0">
         <div style={{textAlign:"center"}}><h3>購買總覽<hr/></h3></div>
-      <div id="cartList">
-        {cart.length === 0 ? (
-        <>
-            <p>購物車是空的</p>
-            <Link to="/products">繼續選購</Link>
-        </>
-        ) : (
-        <>
-            {cart.map(item => (
-            <div key={item.id} className="cartItem">
-                <span>{item.name}</span>
-                <span>x{item.quantity}</span>
-                <span>{item.price * item.quantity} 元</span>
-            </div>
-            ))}
-        </>
-        )}
+        <div id="cartList">
+            {cart.length === 0 ? (
+            <>
+                <p>購物車是空的</p>
+                <Link to="/products">繼續選購</Link>
+            </>
+            ) : (
+            <>
+                {cart.map(item => (
+                <div key={item.id} className="cartItem">
+                    <span>{item.name}</span>
+                    <span>x{item.quantity}</span>
+                    <span>{item.price * item.quantity} 元</span>
+                </div>
+                ))}
+            </>
+            )}
+        </div>
         <div className="cartTotal">
             合計：{totalPrice} 元
         </div>
-      </div>
     </div>
   );
 }
