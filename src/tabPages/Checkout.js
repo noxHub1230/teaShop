a105a5d847a4f6e8f5349adfd5c15fd2c84696f2
noxHub1230 <s110907018@stu.ntue.edu.tw>
2026-05-16 22:18:12 +0800

    checkout頁樣式建立

--- src/tabPages/Checkout.js
+++ src/tabPages/Checkout.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { useCart } from "../components/cart/cart";
 import { Link } from "react-router-dom";
+import "../styles/checkout.css"
 
 export default function Cart() {
   const { cart, totalPrice } = useCart();
 
   if (cart.length === 0) {
     return (
-      <div className="tab-page container-fluid p-0">
+      <div className="BG_checkout tab-page container-fluid p-0">
         <p>購物車是空的</p>
         <Link to="/products">繼續選購</Link>
       </div>
@@ -15,11 +16,10 @@ export default function Cart() {
   }
 
   return (
-    <div className="tab-page container-fluid p-0">
+    <div className="BG_checkout tab-page container-fluid p-0">
       <div id="content_cart">
         {cart.map(item => (
           <div key={item.id} className="cartItem">
-            <img src={item.image} alt={item.name} />
             <span>{item.name}</span>
             <span>x{item.quantity}</span>
             <span>{item.price * item.quantity} 元</span>
